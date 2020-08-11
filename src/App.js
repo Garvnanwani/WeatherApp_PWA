c752e384cb04a98045cce3fd5eeed542469608d6
Garv nanwani <garvnanwani16@gmail.com>
2020-08-11 11:56:20 +0530

    changed the front end part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
+
 
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
-    
+
     const search = async (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             const data = await fetchWeather(query);
 
             setWeather(data);
@@ -16,26 +16,68 @@ const App = () => {
         }
     }
 
+    let d = new Date();
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let dd = String(d.getDate()).padStart(2, '0');
+    let mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = d.getFullYear();
+
+    let date = mm + '/' + dd + '/' + yyyy;
+
+    console.log(weather);
+
     return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
-        </div>
+        <>
+            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+            <div className="container">
+                {weather.main && (
+                    <>
+                        <div className="weather-side">
+                            <div className="weather-gradient"></div>
+                            <div className="date-container">
+                                <h2 className="date-dayname">{weekday[d.getDay()]}</h2><span className="date-day">{date}</span><i className="location-icon" data-feather="map-pin"></i><span className="location">{weather.name}, {weather.sys.country}</span></div>
+                            <div className="weather-container"><i className="weather-icon" data-feather="sun"></i>
+                                <h1 className="weather-temp">{weather.main.temp}°C</h1>
+                                <h3 className="weather-desc">{weather.weather[0].description}</h3>
+                            </div>
+                        </div>
+                        <div className="info-side">
+                            <div className="today-info-container">
+                                <div className="today-info">
+                                    <div className="precipitation"> <span className="title">FEELS LIKE</span><span className="value">{weather.main.feels_like}°C</span>
+                                        <div className="clear"></div>
+                                    </div>
+                                    <div className="humidity"> <span className="title">HUMIDITY</span><span className="value">{weather.main.humidity} %</span>
+                                        <div className="clear"></div>
+                                    </div>
+                                    <div className="wind"> <span className="title">WIND</span><span className="value">{weather.wind.speed} km/h</span>
+                                        <div className="clear"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="week-container">
+                                <ul className="week-list">
+                                    <li className="active"><i className="day-icon" data-feather="sun"></i><span className="day-name">{weekday[d.getDay()]}</span><span className="day-temp">29°C</span></li>
+                                    <li><i className="day-icon" data-feather="cloud"></i><span className="day-name">{weekday[d.getDay() + 1]}</span><span className="day-temp">21°C</span></li>
+                                    <li><i className="day-icon" data-feather="cloud-snow"></i><span className="day-name">{weekday[d.getDay() + 2]}</span><span className="day-temp">08°C</span></li>
+                                    <li><i className="day-icon" data-feather="cloud-rain"></i><span className="day-name">{weekday[d.getDay() + 3]}</span><span className="day-temp">19°C</span></li>
+                                    <div className="clear"></div>
+                                </ul>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
+
     );
 }
 
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const URL = 'https://api.openweathermap.org/data/2.5/forecast';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+
+// api.openweathermap.org / data / 2.5 / forecast ? q = { city name } & appid={ your api key }
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
